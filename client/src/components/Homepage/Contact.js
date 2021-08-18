5b5cfc46fb0013051372d1f024616af73e7826ab
zeldeus12 <up180454@alumnos.upa.edu.mx>
2021-08-18 17:54:09 -0500

    Cambios contacto y css

--- client/src/components/Homepage/Contact.js
+++ client/src/components/Homepage/Contact.js
@@ -1,10 +1,18 @@
 import React from 'react'
-
 function Contact() {
     return (
         <div>
-            <h2 className="subtitulo">Contacto</h2>
-            <center>Información </center>
+            <h2 className="subtitulo" href="https://www.mozilla.org/es-ES/">Contactanos</h2>
+            <h6><link href="style/style_about.css" rel="stylesheet" type="text/css"></link>
+            <center>Oficinas SEDESO</center>
+            <center>Telefono (Conmutador)</center>
+            <center>(449 910 21 21)</center>
+            <center>Ubicación</center>
+            <center>Blvd. José María Chávez No.3205,</center>
+            <center>Cd. Industrial C.P. 20290</center>
+            
+            </h6>
+            
         </div>
     )
 }
