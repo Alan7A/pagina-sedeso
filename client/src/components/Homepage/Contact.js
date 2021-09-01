d86f5149bc745e9a66abaae7ca560e2e878609f1
zeldeus12 <up180454@alumnos.upa.edu.mx>
2021-08-31 20:09:35 -0500

    Actualizacion Sin cambios

--- client/src/components/Homepage/Contact.js
+++ client/src/components/Homepage/Contact.js
@@ -4,15 +4,16 @@ function Contact() {
         <div>
             <h2 className="subtitulo" href="https://www.mozilla.org/es-ES/">Contactanos</h2>
             <div className="contacto">
-                <center>Oficinas SEDESO</center>
-                <center>Telefono (Conmutador)</center>
-                <center>449 910 21 21</center>
-                <center>Ubicación</center>
-                <center>Blvd. José María Chávez No.3205,</center>
-                <center>Cd. Industrial C.P. 20290</center>
+
             </div>
         </div>
     )
 }
 
 export default Contact;
+                {/* <center>Oficinas SEDESO</center>
+                <center>Telefono (Conmutador)</center>
+                <center>449 910 21 21</center>
+                <center>Ubicación</center>
+                <center>Blvd. José María Chávez No.3205,</center>
+                <center>Cd. Industrial C.P. 20290</center> */}
--- client/src/components/Homepage/HomepageScreen.js
+++ client/src/components/Homepage/HomepageScreen.js
@@ -5,14 +5,13 @@ import Contact from './Contact'
 import HeroImage from './HeroImage'
 import Map from './Map'
 import './styles.css'
-
 function HomepageScreen() {
     return (
+
         <div style={{ backgroundColor: '#fafafa' }}>
+            
             <HeroImage /> 
-            {/* Aqui inicia la Imagen portada */}
-           
-            {/* Aqui termina la Imagen portada */}
+
             <Container maxWidth='md'>
                 <h2 className="subtitulo">Centros contigo</h2>
                 <CentrosContigoTable />
@@ -27,3 +26,13 @@ function HomepageScreen() {
 }
 
 export default HomepageScreen
+
+// let slides = [
+//     <img  src="https://picsum.photos/800/300/?random" alt="1" />,
+//     <img  src="https://picsum.photos/800/301/?random" alt="2" />  ,
+//     <img  src="https://picsum.photos/800/302/?random" alt="3" />  ,
+//     <img  src="https://picsum.photos/800/303/?random" alt="4" />  ,
+//     <img src="https://picsum.photos/800/304/?random" alt="5" />   ];
+// Ignorar Esto lo usare despues Att. gabriel
+// <Carousel slides={slides} autoplay={true} interval={1000}/>
+
