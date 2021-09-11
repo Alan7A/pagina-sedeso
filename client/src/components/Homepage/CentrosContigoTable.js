cf55070b911cfefc7be5ddcdcb5b65edf3191ac6
Alan Delgado <7a.alan@gmail.com>
2021-09-11 01:44:22 -0500

    Correcciones en tabla centros crecer y package.json

--- client/src/components/Homepage/CentrosContigoTable.js
+++ client/src/components/Homepage/CentrosContigoTable.js
@@ -1,8 +1,7 @@
-import { Paper, Table, TableContainer, TableHead, TableRow, TableBody, TableCell, withStyles, Button, Modal } from '@material-ui/core'
+import { Paper, Table, TableContainer, TableHead, TableRow, TableBody, TableCell, withStyles, Button, Modal, Tooltip, IconButton } from '@material-ui/core'
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
+import Delete from '@material-ui/icons/Delete';
 import AddIcon from '@material-ui/icons/Add';
 import SaveAltIcon from '@material-ui/icons/SaveAlt';
 import CloseIcon from '@material-ui/icons/Close';
@@ -26,7 +25,6 @@ function CentrosContigoTable() {
     const { idCentro } = useParams();
     const styles = useStyles();
     const [modalAdd, setModalAdd] = useState(false);
-    const [modalEdit, setModalEdit] = useState(false);
     const [error, setError] = useState(false);
     const [crearCentro, setCrearCentro] = useState(initialFormValues);
     const [imagenes, setImagenes] = useState([]);
@@ -39,42 +37,42 @@ function CentrosContigoTable() {
     }
 
     // Extraer los valores
-    const {nom, ub, tel} = crearCentro;
+    const { nom, ub, tel } = crearCentro;
 
     // Eliminar Centro
     const onDeleteCentro = async (e) => {
         await axios.delete(`/CentrosContigo/eliminar/${e}`, headers);
     }
-    
+
     // Agregar Centro
     const onSubmitCentro = async (e) => {
         e.preventDefault();
 
-            if (nom.trim() === '' || ub.trim() === '' || tel.trim() === ''){
-                setError(true);
-                return;
-            }
+        if (nom.trim() === '' || ub.trim() === '' || tel.trim() === '') {
+            setError(true);
+            return;
+        }
 
-            // Convertir files a base64 para poder subir a la bd
-            const imagenesBase64 = await Promise.all(imagenes.map(async (imagen) => await fileToBase64(imagen)))
-            // Hacer que las imágenes tengan el formato que acepta mysql
-            const nuevasImagenes = imagenesBase64.map((imagen) => (Object.values({ imagen })))
+        // Convertir files a base64 para poder subir a la bd
+        const imagenesBase64 = await Promise.all(imagenes.map(async (imagen) => await fileToBase64(imagen)))
+        // Hacer que las imágenes tengan el formato que acepta mysql
+        const nuevasImagenes = imagenesBase64.map((imagen) => (Object.values({ imagen })))
 
-            // Agregar los horarios y las imágenes al objeto que se manda en el body
-            let data = crearCentro;
-            data.imagenes = nuevasImagenes;
+        // Agregar los horarios y las imágenes al objeto que se manda en el body
+        let data = crearCentro;
+        data.imagenes = nuevasImagenes;
 
-            setError(false);
+        setError(false);
 
-            await axios.post(`/CentrosContigo/agregarCentro`, data, headers);
+        await axios.post(`/CentrosContigo/agregarCentro`, data, headers);
 
-            abrirCerrarAdd();
+        abrirCerrarAdd();
 
-            setCrearCentro({
-                nom: '',
-                ub: '',
-                tel: ''
-            })
+        setCrearCentro({
+            nom: '',
+            ub: '',
+            tel: ''
+        })
     }
 
     useEffect(() => {
@@ -103,10 +101,6 @@ function CentrosContigoTable() {
         })
     }
 
-    const abrirCerrarEdit = () => {
-        setModalEdit(!modalEdit);
-    }
-    
     const ModalAdd = (
         <div className={styles.modal}>
             <div className="center">
@@ -115,136 +109,107 @@ function CentrosContigoTable() {
             <form
                 onSubmit={onSubmitCentro}
             >
-            <TextField 
-                type="text" 
-                label="Nombre del Centro" 
-                name="nom" 
-                value={nom} 
-                onChange={onChangeCentro} 
-                className={styles.texttield} 
-            />
-            <br />
-            <TextField 
-                type="text"
-                label="Ubicación" 
-                name="ub" 
-                value={ub} 
-                onChange={onChangeCentro} 
-                className={styles.texttield} 
-            />
-            <br />
-            <TextField 
-                type="tel"
-                label="Teléfono" 
-                name="tel" 
-                value={tel} 
-                onChange={onChangeCentro} 
-                className={styles.texttield} 
-            />
-            <br /> <br />
-            <div align="right">
-                <Button variant="contained" color="primary" type="submit" onChange={onChangeCentro} className={styles.button}>
-                    <SaveAltIcon></SaveAltIcon>
-                </Button>
-                <Button variant="contained" color="primary" className={styles.button} onClick={() => abrirCerrarAdd()}>
-                    <CloseIcon></CloseIcon>
-                </Button>
-            </div>
-            <DropzoneArea
-                acceptedFiles={['image/*']}
-                initialFiles={imagenes}
-                dropzoneText={"Arrastra tus imágenes o da clic aquí para subir imágenes (máximo 9)"}
-                onChange={(files) => setImagenes(files)}
-                filesLimit={9}
-                getFileLimitExceedMessage={(filesLimit) => `Solo se permiten ${filesLimit} imágenes como máximo`}
-                getFileAddedMessage={(fileName) => `Imagen ${fileName} agregada`}
-                getFileRemovedMessage={(fileName) => `Imagen ${fileName} eliminada`}
-                getDropRejectMessage={(rejectedFile, acceptedFiles, maxFileSize) => {
-                    let message = `No Se pudo subir ${rejectedFile.name}. `;
-                    if (acceptedFiles.includes(rejectedFile.type)) {
-                        message += 'Solo puedes subir imagenes. ';
-                    }
-                    if (rejectedFile.size > maxFileSize) {
-                        message += 'La imagen es demasiado pesada, el tamaño máximo es 3MB.';
-                    }
-                    return message;
-                }}
-            />
+                <TextField
+                    type="text"
+                    label="Nombre del Centro"
+                    name="nom"
+                    value={nom}
+                    onChange={onChangeCentro}
+                    className={styles.texttield}
+                />
+                <br />
+                <TextField
+                    type="text"
+                    label="Ubicación"
+                    name="ub"
+                    value={ub}
+                    onChange={onChangeCentro}
+                    className={styles.texttield}
+                />
+                <br />
+                <TextField
+                    type="tel"
+                    label="Teléfono"
+                    name="tel"
+                    value={tel}
+                    onChange={onChangeCentro}
+                    className={styles.texttield}
+                />
+                <br /> <br />
+                <div align="right">
+                    <Button variant="contained" color="primary" type="submit" onChange={onChangeCentro} className={styles.button}>
+                        <SaveAltIcon></SaveAltIcon>
+                    </Button>
+                    <Button variant="contained" color="primary" className={styles.button} onClick={() => abrirCerrarAdd()}>
+                        <CloseIcon></CloseIcon>
+                    </Button>
+                </div>
+                <DropzoneArea
+                    acceptedFiles={['image/*']}
+                    initialFiles={imagenes}
+                    dropzoneText={"Arrastra tus imágenes o da clic aquí para subir imágenes (máximo 9)"}
+                    onChange={(files) => setImagenes(files)}
+                    filesLimit={9}
+                    getFileLimitExceedMessage={(filesLimit) => `Solo se permiten ${filesLimit} imágenes como máximo`}
+                    getFileAddedMessage={(fileName) => `Imagen ${fileName} agregada`}
+                    getFileRemovedMessage={(fileName) => `Imagen ${fileName} eliminada`}
+                    getDropRejectMessage={(rejectedFile, acceptedFiles, maxFileSize) => {
+                        let message = `No Se pudo subir ${rejectedFile.name}. `;
+                        if (acceptedFiles.includes(rejectedFile.type)) {
+                            message += 'Solo puedes subir imagenes. ';
+                        }
+                        if (rejectedFile.size > maxFileSize) {
+                            message += 'La imagen es demasiado pesada, el tamaño máximo es 3MB.';
+                        }
+                        return message;
+                    }}
+                />
             </form>
             {error ? <p className="mensaje error">Todos los campos son obligatorios</p> : null}
         </div>
     )
 
-    const ModalEdit = (
-        <div className={styles.modal}>
-            <div className="center">
-                <h2>Editar centro contigo</h2>
-            </div>
-            <TextField label="Nombre del Centro" className={styles.texttield} />
-            <br />
-            <TextField label="Ubicación" className={styles.texttield} />
-            <br />
-            <TextField label="Teléfono" className={styles.texttield} />
-            <br /> <br />
-            <div align="right">
-                <Button variant="contained" color="primary" className={styles.button}>
-                    <SaveAltIcon></SaveAltIcon>
-                </Button>
-                <Button variant="contained" color="primary" className={styles.button} onClick={() => abrirCerrarEdit()}>
-                    <CloseIcon></CloseIcon>
-                </Button>
-            </div>
-        </div>
-    )
-
     return (
         <Fragment>
-        <TableContainer component={Paper} style={{height: 480}}>
-            <Table stickyHeader>
-                <TableHead>
-                    <TableRow>
-                        <StyledTableCell>Centro Contigo</StyledTableCell>
-                        <StyledTableCell>Ubicación</StyledTableCell>
-                        <StyledTableCell></StyledTableCell>
-                        <StyledTableCell>
-                        <Fab size="small" aria-label="add" className="" onClick={() => abrirCerrarAdd()}>
-                            <AddIcon />
-                        </Fab>
-                        </StyledTableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody> 
-                    {centros.map((centro, i) =>
-                        <StyledTableRow key={i}>
-                            <StyledTableCell onClick={() => handleClick(centro.id)}>{centro.CentroContigo}</StyledTableCell>
-                            <StyledTableCell onClick={() => handleClick(centro.id)}>{centro.Ubicación}</StyledTableCell>
+            <TableContainer component={Paper} style={{ height: 480 }}>
+                <Table stickyHeader>
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Centro Contigo</StyledTableCell>
+                            <StyledTableCell>Ubicación</StyledTableCell>
                             <StyledTableCell>
-                                <Button variant="contained" color="primary" onClick={() => abrirCerrarEdit()}>
-                                    <EditIcon></EditIcon>
-                                </Button>
+                                <Fab size="small" aria-label="add" className="" onClick={() => abrirCerrarAdd()}>
+                                    <AddIcon />
+                                </Fab>
                             </StyledTableCell>
-                            <StyledTableCell>
-                                <Button variant="contained" color="primary" onClick={() => onDeleteCentro(centro.id)}>
-                                    <DeleteIcon></DeleteIcon>
-                                </Button>
-                            </StyledTableCell>
-                        </StyledTableRow>
-                    )}
-                </TableBody>
-            </Table>
-            
-        </TableContainer>
-        <Modal
-            open={modalAdd}
-            onClose={abrirCerrarAdd}>
-                {ModalAdd}
-        </Modal>
-        <Modal
-            open={modalEdit}
-            onClose={abrirCerrarEdit}>
-                {ModalEdit}
-        </Modal>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {centros.map((centro, i) =>
+                            <StyledTableRow key={i}>
+                                <StyledTableCell onClick={() => handleClick(centro.id)}>{centro.CentroContigo}</StyledTableCell>
+                                <StyledTableCell onClick={() => handleClick(centro.id)}>{centro.Ubicación}</StyledTableCell>
+                                <StyledTableCell>
+                                    <Tooltip title='Eliminar' onClick={() => onDeleteCentro(centro.id)}>
+                                        <IconButton style={{ color: '#b00020' }}>
+                                            <Delete />
+                                        </IconButton>
+                                    </Tooltip>
+                                    {/* <Button variant="contained" color="primary" onClick={() => onDeleteCentro(centro.id)}>
+                                        <DeleteIcon></DeleteIcon>
+                                    </Button> */}
+                                </StyledTableCell>
+                            </StyledTableRow>
+                        )}
+                    </TableBody>
+                </Table>
 
+            </TableContainer>
+            <Modal
+                open={modalAdd}
+                onClose={abrirCerrarAdd}>
+                {ModalAdd}
+            </Modal>
         </Fragment>
     )
 }
