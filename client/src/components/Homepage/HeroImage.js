55b49d4b8bab9b30fb5761c47c42c7b2d8874e15
Ivan Delfino <ivanjose150600@gmail.com>
2021-09-21 21:26:43 -0500

    Solicito Imagenes de carrousel

--- client/src/components/Homepage/HeroImage.js
+++ client/src/components/Homepage/HeroImage.js
@@ -1,24 +1,45 @@
-import React from 'react'
 import { Typography } from '@material-ui/core';
 import portada from './portada.jpg'
+import ImageGallery from 'react-image-gallery';
+import axios from '../../utils/axios';
+import React, { useEffect, useState } from 'react';
+
 // import {Carousel} from '3d-react-carousal'  No borrar att: Gabriel;
 
+
+
+
+
 function HeroImage() {
+  const [imagenes, setImg] = useState([]);
+
+  useEffect(() => {
+    const getImagenes = async () => {
+        try {
+            const response = await axios.get(`/imagenes/`);
+            setImg(response.data);
+            console.log("AQUI DEBERIA DE IR EL ARRAY DE IMAGENES");
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    getImagenes();
+  }, []);
+
+  
     return (
-        <div style={image} className='hero-image'>
+      <div style={image} className='hero-image'>
 
     
             <div className='hero-text'>
                 <Typography variant='h4' component='h1'>Secretaría de Desarrollo Social</Typography>
             </div>
         </div>
+  
     )
 }
-
 const image = {
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${portada})`,
-
+  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${portada})`,
 }
-
-
 export default HeroImage
