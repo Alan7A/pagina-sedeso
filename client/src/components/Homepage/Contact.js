1e45d1d671255ce2f3ffd4ddb4a40e35101795de
zeldeus12 <up180454@alumnos.upa.edu.mx>
2021-09-30 13:01:56 -0500

    Cambios en contact codigo

--- client/src/components/Homepage/Contact.js
+++ client/src/components/Homepage/Contact.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Col, Form, FormGroup, Label, Input } from 'reactstrap';
-
 
 function Contact() {
     return (
         <div>
             <h2 className="subtitulo" href="https://www.mozilla.org/es-ES/">Contactanos</h2>
             <div className="contacto">
-
-                <h6> <center>Telefono (Conmutador)</center>
-                <center>449 910 21 21</center>
-                <center>Oficinas SEDESO</center>
-                <center>Ubicación</center>
-                <center>Blvd. José María Chávez No.3205,</center> 
-                 <center>Cd. Industrial C.P. 20290</center> </h6>
+                <div className='contenedor-Contact'>
+                    <center>Telefono (Conmutador)</center>
+                    <center>449 910 21 21</center>
+                    <center>Oficinas SEDESO</center>
+                    <center>Ubicación</center>
+                    <center>Blvd. José María Chávez No.3205,</center>
+                    <center>Cd. Industrial C.P. 20290</center>
+                </div>
             </div>
         </div >
     )
