194f87f7e2a7d1f906369c2d4cac4ee5357c2c12
zeldeus12 <up180454@alumnos.upa.edu.mx>
2021-10-24 20:24:00 -0500

    Hermoso Contactanos

--- client/src/components/Homepage/Contact.js
+++ client/src/components/Homepage/Contact.js
@@ -1,25 +1,59 @@
 import React from 'react';
-import llam from './Ags.png'
+import "bootstrap/dist/css/bootstrap.min.css"
 function Contact() {
     return (
         <div>
             <h2 className="subtitulo" href="https://www.mozilla.org/es-ES/">Contactanos</h2>
             <div className="contacto">
-                <div className='contenedor-image'>
-                <center><img src={llam} align="right" width="150" height="150"/></center>
-
+                {/* <div className='contenedor-Contact-derecha' >
+                    <img src={Phone} width="60" height="30" />
+                    <p>Telefono (Conmutador)</p>
+                    <p>449 910 21 21</p>
+                    <p>Oficinas SEDESO</p>
+                    <div className='contenedor-Contact-izquierda' >
+                        <img src={Location} width="60" height="30" />
+                        <p>UBICACIÓN</p>
+                        <p>Blvd. José María Chávez No.3205
+                        Cd. Industrial C.P. 20290
+                    </div> */}
+                    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'/>
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+                <div class="row mb-5">
+                    <div class="col-md-3">
+                        <div class="dbox w-100 text-center">
+                            <div class="icon d-flex align-items-center justify-content-center">
+                                <span class="fa fa-map-marker"></span>
+                            </div>
+                            <div class="text">
+                                <p><span>Dirección:</span> Blvd. José María Chávez No.3205
+                        Cd. Industrial C.P. 20290</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="dbox w-100 text-center">
+                            <div class="icon d-flex align-items-center justify-content-center">
+                                <span class="fa fa-phone"></span>
+                            </div>
+                            <div class="text">
+                                <p><span>Telefono:</span> <a href="tel://449 910 21 21">449 910 21 218 </a> Ext.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="dbox w-100 text-center">
+                            <div class="icon d-flex align-items-center justify-content-center">
+                                <span class="fa fa-paper-plane"></span>
+                            </div>
+                            <div class="text">
+                                <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className='contenedor-Contact'>
-                    <center>Telefono (Conmutador)</center>
-                    <center>449 910 21 21</center>
-                    <center>Oficinas SEDESO</center>
-                    <center>UBICACIÓN</center>
-                    <center>Blvd. José María Chávez No.3205,</center>
-                    <center>Cd. Industrial C.P. 20290</center>
-                </div>
-                
+   
             </div>
-        </div >
+        </div>
     )
 }
 
